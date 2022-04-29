09a30af4d1b6929bb50defb5ad33d5c068caef36
HenbiPY <leofreddie123@hotmail.com>
2022-04-29 14:26:42 -0400

    migrated landingpage.js to index.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,26 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { NavigationConstant } from '../components/NavigationBar'
-import styles from '../styles/Home.module.scss'
+import styles from '../styles/Landing.module.scss'
+import { NavigationConstant } from "../components/NavigationBar";
 
-const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>TGA</title>
-        <link rel="icon" href="/eye.svg" />
-      </Head>
+const Home:NextPage = () => {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Landing Page</title>
+                <link rel="icon" href="/eye.svg" />
+            </Head>
 
-      <main className={styles.main}>
-        <NavigationConstant />
-        <h1 className={styles.title}>
-            Welcome to <em>The Green Alliance</em>!
-        </h1>
-      </main>
-    </div>
-  )
+            <main className={styles.main}>
+                 <NavigationConstant />
+                 <h1 className={styles.welcome}>Welcome To</h1>
+                <h1 className={styles.title}>The Green Alliance</h1>
+                <div className={styles.divider}> </div>
+                <h2 className={styles.subtitle}>Our Mission</h2>
+            </main>
+        </div>
+    )
 }
 
 export default Home
