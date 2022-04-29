f3c82d725a368bd1e95cf576e476394737f002f6
HenbiPY <leofreddie123@hotmail.com>
2022-04-29 14:27:30 -0400

    oops wrong style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Landing.module.scss'
+import styles from '../styles/Home.module.scss'
 import { NavigationConstant } from "../components/NavigationBar";
 
 const Home:NextPage = () => {
