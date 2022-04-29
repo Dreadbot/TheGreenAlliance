cea07c756ce229895bc6ca13fa8a9cea60de2a7d
pantaelaman <calvin@theophoffs.com>
2022-04-29 14:32:34 -0400

    Fixed code format

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.scss';
 import { NavigationConstant } from "../components/NavigationBar";
 
-const Home:NextPage = () => {
+const Home: NextPage = () => {
     return (
         <div className={styles.container}>
             <Head>
@@ -13,14 +13,14 @@ const Home:NextPage = () => {
             </Head>
 
             <main className={styles.main}>
-                 <NavigationConstant />
-                 <h1 className={styles.welcome}>Welcome To</h1>
+                <NavigationConstant />
+                <h1 className={styles.welcome}>Welcome To</h1>
                 <h1 className={styles.title}>The Green Alliance</h1>
                 <div className={styles.divider}> </div>
                 <h2 className={styles.subtitle}>Our Mission</h2>
             </main>
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;
