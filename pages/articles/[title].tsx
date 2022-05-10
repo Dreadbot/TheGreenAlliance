1c8f8dd2bc2b9f8e94dee980e804c7505a88b595
KCsup <kcsup05@gmail.com>
2022-05-10 12:51:39 -0400

    Changed to server side rendering, basis of MD conversion made.

--- pages/articles/[title].tsx
+++ pages/articles/[title].tsx
@@ -1,9 +1,7 @@
-import { GetStaticPaths, GetStaticProps, NextPage } from "next";
-import { useRouter } from "next/router";
-import { Document, MongoClient, WithId } from 'mongodb';
+import { MongoClient } from 'mongodb';
+import { GetServerSideProps } from "next";
 import { NavigationConstant } from "../../components/NavigationBar";
 import styles from "../../styles/Article.module.scss";
-import fs from "fs"
 
 type ArticleType = {
     article: {
@@ -21,17 +19,19 @@ const Article = ( { article }: ArticleType ) => {
     )
 }
 
-const getArticleHtml = (article: { title: string; text: any; }) => {
+const getArticleHtml = (article: { title: string; text: string; }) => {
     const showdown = require('showdown')
     const converter = new showdown.Converter()
+    const text = article.text
+    console.log(text)
     return article != null ? {
-        __html: converter.makeHtml(article.text)
+        __html: converter.makeHtml(text)
     } : {
         __html: "<h1>NA</h1>"
     }
 }
 
-export const getStaticProps: GetStaticProps = async (context) => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
     const { title } = context.params as { title: string }
     const file = require("../../private/mongo.json")
     const url = `mongodb+srv://${file.username}:${file.password}@thegreenalliance.5xwjk.mongodb.net/thegreenalliance?retryWrites=true&w=majority`
@@ -52,13 +52,6 @@ export const getStaticProps: GetStaticProps = async (context) => {
         props: {
             article: null
         }
-    }
-}
-
-export const getStaticPaths: GetStaticPaths<{ title: string }> = async () => {
-    return {
-        paths: [],
-        fallback: 'blocking'
     }
 }
 
